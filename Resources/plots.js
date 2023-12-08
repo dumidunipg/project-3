4b15f8ff34c862bfe0699c4fa37a71ee3e7e8d6a
shruti-p-m <shruti.mallampalli@gmail.com>
2023-12-07 19:57:17 -0800

    reorganizing structure

---
+++ Resources/plots.js
@@ -0,0 +1,14 @@
+/* fetch('http://127.0.0.1:5000/api/data').then(data => {
+    console.log(data);
+}).catch(error => console.error('Error:', error)); */
+
+sea_url = 'sea_temp.json'
+co2_url = 'co2.json'
+function init() {
+
+    d3.json(sea_url).then(function(data){
+        
+    });
+};
+
+init();

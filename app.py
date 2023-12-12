4956d2633016872e3b1ed9522d6eadc3ecae9b03
shruti-p-m <shruti.mallampalli@gmail.com>
2023-12-11 18:46:25 -0800

    updating app.py and index.html to v5 of d3

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 # Import the dependencies.a
 from flask import Flask, jsonify
 from flask_pymongo import PyMongo
-import pandas as pd
+
 
 # Create an app, being sure to pass __name__
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/pollution"
 mongo = PyMongo(app)
-
 #
 @app.route("/")
 def home():
@@ -22,13 +21,20 @@ def home():
 
     )
 
-@app.route("/air-quality") #need to delete
-def air_quality():
-    df = pd.read_csv("./Resources/who_ambient_air_quality.csv")
-    columns_selection = ["country_name","pm25_concentration","year"]
-    clean_df = df[columns_selection].dropna(subset=["pm25_concentration"])
-    gb = clean_df.groupby(["country_name","year"]).mean().reset_index()
-    return jsonify (gb.to_dict("records"))
+@app.route('/api/data/seatemp', methods=['GET'])
+def get_data_seatemp():
+    data = list(mongo.db.sea_temp.find({}, {'_id': 0}))
+    return jsonify(data)
+
+@app.route('/api/data/co2', methods=['GET'])
+def get_data_co2():
+    data = list(mongo.db.co2_emission.find({}, {'_id': 0}))
+    return jsonify(data)
+
+@app.route('/api/data/aqi', methods=['GET'])
+def get_data_aqi():
+    data = list(mongo.db.aqi.find({}, {'_id': 0}))
+    return jsonify(data)
 
 @app.route('/api/data/seatemp', methods=['GET'])
 def get_data_seatemp():
